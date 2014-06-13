31cc4b29cc812a4bdfef896411e0a4f30d9aa92d
Chris lamb <cclamb@ece.unm.edu>
2014-06-13 11:10:53 -0600

    work

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -6,6 +6,21 @@ Todos.TodoController = Ember.ObjectController.extend({
 
 		editTodo: function() {
 			this.set('isEditing', true);
+		},
+
+		acceptChanges: function() {
+			this.set('isEditing', false);
+			if (Ember.isEmpty(this.get('model.title'))) {
+				this.send('removeTodo');
+			} else {
+				this.get('model').save()
+			}
+		},
+
+		removeTodo: function() {
+			var todo = this.get('model');
+			todo.deleteRecord();
+			todo.save();
 		}
 
 	},
